01bb5d23df2c5f53e063ddd7c8962df7aa2d1913
robbin256 <robbinvanvliet@icloud.com>
2025-09-22 11:41:34 +0200

    Ready for Merge

--- bestelpagina.php
+++ bestelpagina.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/bestel.css" defer>
+    <link rel="stylesheet" href="assetsRobbin/css/bestel.css" defer>
     <script src="bestelpagina.js" defer></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Document</title>
